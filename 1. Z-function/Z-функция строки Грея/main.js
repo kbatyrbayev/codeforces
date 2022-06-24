48c0c36c83e97a3e57f3bf314e2b0a223313e0a5
Kairat Batyrbayev <k.batyrbayev>
2022-06-24 22:48:11 +0600

    Z-функция строки Грея

--- 1. Z-function/Z-функция строки Грея/main.js
+++ 1. Z-function/Z-функция строки Грея/main.js
@@ -1,29 +1,23 @@
-const {readline, print} = require('@ip-algorithmics/codeforces-io');
+const { readline, print } = require('@ip-algorithmics/codeforces-io');
 
-var x = parseInt(readline());
-
+var k = parseInt(readline());
+ 
 var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-var gArr = [];
-
-for (var i = 1; i <= alphabet.length - 1; i++) {
-    if (i === 1) {
-        gArr[i - 1] = 'a';
-    } else {
-        gArr[i - 1] = gArr[i - 2] + alphabet[i - 1] + gArr[i - 2];
-    }
+ 
+var gArr = ['a'];
+for (var i = 2; i <= 26; i++) {
+    gArr[i - 1] = gArr[i - 2] + alphabet[i - 1] + gArr[i - 2];
 }
-
-/*for (var l = 0; l < x; l++) {
-    var line =
-}*/
-
-var k = 3;
-var j = 3;
-var g = gArr[k-1];
-var res = compareStrs(g, g.substring(j, g.length));
-print(res);
-
-
+ 
+var res = [];
+for (var l = 0; l < k; l++) {
+    var line = readline().split(' ').map(r => +r);
+    var g = gArr[line[0] - 1];
+    res[l] = compareStrs(g, g.substring(line[1], g.length));
+}
+ 
+print(res.join(' \n'));
+ 
 function compareStrs(str1, str2) {
     var cnt = 0;
     for (var i = 0; i < str1.length; i++) {
